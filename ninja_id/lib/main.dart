bd0ba8ddc23242422bc00b60232f9e997e34e45f
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-06-20 21:46:16 +0800

    convert stateless widget to stateful widget

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -7,8 +7,16 @@ void main() {
   ));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({super.key});
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +30,20 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            ninjaLevel++;
+          });
+        },
+        backgroundColor: Colors.grey[800],
+        shape: CircleBorder(),
+        child: Icon(
+            Icons.add,
+            color: Colors.white,
+        ),
       ),
       body: Padding(
             padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -65,7 +87,7 @@ class NinjaCard extends StatelessWidget {
                 ),
                 SizedBox(height: 10),
                 Text(
-                  "8",
+                  '$ninjaLevel',
                   style: TextStyle(
                     color: Colors.amberAccent[200],
                     letterSpacing: 2.0,
